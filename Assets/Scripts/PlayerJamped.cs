5e3eb19f3828110e9d865f530f3d7c219ec6f49a
Mitsuki Sawano <m01161594a@edu.teu.ac.jp>
2018-11-05 19:08:20 +0900

    11.05.2018 Gyrotest
    This is Gyrotest.
    I want to you try to play it.
    
    ジャイロ機能のテストです
    遊んでみてください。

---
+++ Assets/Scripts/PlayerJamped.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJamped : MonoBehaviour {
+
+    [SerializeField] float jumpPower = 100f;
+    bool jump;
+    private Rigidbody2D rb;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        bool jump = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0) && !jump)
+        {
+            rb.AddForce(Vector2.up * jumpPower);
+            jump = true;
+        }
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            jump = false;
+        }
+    }
+}
